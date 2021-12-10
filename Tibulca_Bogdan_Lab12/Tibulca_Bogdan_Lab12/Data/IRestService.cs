6ae0f62412ce9b3499d26abdd95169d89c60f8b1
BogdanTibulca <bogdantibulca@yahoo.com>
2021-12-10 19:49:48 +0200

    Add project files.

---
+++ Tibulca_Bogdan_Lab12/Tibulca_Bogdan_Lab12/Data/IRestService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Tibulca_Bogdan_Lab12.Models;
+
+namespace Tibulca_Bogdan_Lab12.Data
+{
+    public interface IRestService
+    {
+        Task<List<ShopList>> RefreshDataAsync();
+        Task SaveShopListAsync(ShopList item, bool isNewItem);
+        Task DeleteShopListAsync(int id);
+    }
+}
